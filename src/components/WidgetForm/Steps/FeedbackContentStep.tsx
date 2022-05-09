712090e4caeb233f70d8a6bce1d30c9fa8bfd4c6
Ricardo Lopes <prslopes1@gmail.com>
2022-05-09 11:53:29 -0300

    Layout done

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,58 @@
+import { ArrowLeft, ArrowRight, Camera } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton"
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+  
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  
+  return (
+    <>
+      <header>
+        <button 
+          type="button" 
+          className="top-5 left-5  absolute text-zinc-400 hover:text-zinc-100"
+          onClick={() => onFeedbackRestartRequested()}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+
+
+        <span className="text-xl leading-6 flex items-center gap-2" >
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          {feedbackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+
+      <form className="my-4 w-full ">
+        <textarea 
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="bg-zinc-800 border-transparent rounded-md p-2 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          >
+          <Camera className="w-6 h-6" />
+          </button>
+            
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 border-transparent rounded-md flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback <ArrowRight weight="bold" className="w-4 h-4" />
+          </button>
+        </footer>
+      </form>
+    </>
+  )
+}
---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,62 @@
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from "../../assets/bug.png";
+import ideaImageUrl from "../../assets/idea.png";
+import thoughtImageUrl from "../../assets/thought.png";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto"
+    }
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "imagem de uma lâmpada"
+    }
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de mensamento"
+    }
+  }
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChaged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep 
+        feedbackType={feedbackType} 
+        onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+      
+
+      <footer className="Text-xs text-neutral-400">
+        Feito com carinho por <a href="https://github.com/RicardoLopes1" className="underline underline-offset-2 text-blue-500" target="_blank">Ricardo Lopes</a>
+      </footer>
+    </div>
+  )
+}

cab6b6ac29e91f4fe6309192dc814311398d667a
Ricardo Lopes <prslopes1@gmail.com>
2022-05-16 10:38:17 -0300

    Adjustment for frontend deployment done

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, ArrowRight } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -14,14 +16,22 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
   
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState("");
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
   
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault();
-    console.log({ screenshot, comment });
+    setIsSendingFeedback(true);
+
+    await api.post("/feedbacks", {
+      type: feedbackType,
+      comment: comment,
+      screenshot: screenshot,
+    });
 
     onFeedbackSent();
+    setIsSendingFeedback(false);
   }
 
   return (
@@ -34,7 +44,6 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
         >
           <ArrowLeft weight="bold" className="w-4 h-4" />
         </button>
-
 
         <span className="text-xl leading-6 flex items-center gap-2" >
           <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
@@ -59,10 +68,16 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 border-transparent rounded-md flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback <ArrowRight weight="bold" className="w-4 h-4" />
+            {isSendingFeedback ? (
+             <Loading />
+            ) : (
+              <>
+                Enviar feedback <ArrowRight weight="bold" className="w-4 h-4" />
+              </>
+            )} 
           </button>
         </footer>
       </form>
